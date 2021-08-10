e7d45864611568a8d71a2f5f0972c1b033c9f0ee
vessos <vinsent_vsm@abv.bg>
2021-08-10 22:26:02 +0300

    started commit

---
+++ src/main/java/guru/springframework/spring5littleapp/Spring5LittleAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5littleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5LittleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5LittleAppApplication.class, args);
+    }
+
+}
